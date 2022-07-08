522503509b16ce1a13e58383592c5a75119de933
Daniel Kondratiuk <venga1.ua@gmail.com>
2022-07-08 19:58:02 +0200

    slider

---
+++ scripts/utils/slider.js
@@ -0,0 +1,45 @@
+function slider(media, index) {
+    const slideInfo = media.map(({image, video, title}) => {
+        return {title, type: image && !video ? 'image' : 'video', url: image || video}
+    })
+    const slider = document.querySelector('.slider')
+    const sliderTitle = slider.querySelector('.slider__title')
+    const sliderImg = slider.querySelector('.slider__img')
+    const sliderVideo = slider.querySelector('.slider__video')
+    const prev = slider.querySelector('.prev')
+    const next = slider.querySelector('.next')
+    const close = slider.querySelector('.close')
+
+    function makeSliderByType() {
+        if (slideInfo[index].type === 'video') {
+            sliderImg.style.display = 'none'
+            sliderVideo.style.display = 'block'
+            sliderVideo.src = `./assets/videos/${slideInfo[index].url}`
+        } else {
+            sliderVideo.style.display = 'none'
+            sliderImg.style.display = 'block'
+            sliderImg.src = `./assets/images/${slideInfo[index].url}`
+        }
+    }
+
+    slider.classList.add('open')
+    sliderTitle.innerHTML = slideInfo[index].title
+    makeSliderByType()
+
+    function changeSlide(direction) {
+        if (direction === 'prev') {
+            index = index === 0 ? slideInfo.length - 1 : index - 1
+        }
+        if (direction === 'next') {
+            index = index === slideInfo.length - 1 ? 0 : index + 1
+        }
+        makeSliderByType()
+        sliderTitle.innerHTML = slideInfo[index].title
+    }
+
+    prev.addEventListener('click', () => changeSlide('prev'))
+    next.addEventListener('click', () => changeSlide('next'))
+    close.addEventListener('click', () => slider.classList.remove('open'))
+}
+
+export default slider
